78267350819ad7542f370da8c70f9cec00b8cec0
fabarm <leonovxai@gmail.com>
2022-02-17 12:23:28 +0200

    added creditDutyAllCustomers

--- bank.js
+++ bank.js
@@ -1,17 +1,16 @@
 let bank = [];
 
-async function getExchangeRates (url) {
-  let res = await fetch(url);
+async function getExchangeRates() {
+  let res = await fetch("https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=5");
 
   if (!res.ok) {
-    throw new Error(`Could not fetch ${url}, status: ${res.status}`);
+    throw new Error(`Could not fetch , status: ${res.status}`);
   }
 
   return await res.json();
 }
-// let a1 = getExchangeRates("https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=5")
-// console.log(a1)
-
+let a = getExchangeRates();
+console.log(a)
 class Customer {
   fullName;
   isActive;
@@ -23,47 +22,73 @@ class Customer {
     this.fullName = fullName;
     this.isActive = isActive;
     this.dateReg = dateReg;
-    this.debitAccount = {}
+    this.debitAccount = [];
+    this.creditAccount = [];
   }
-  setDebitAccount (debitAccountN, dateEndActive, debitBalance, debitCurrency) {
-    this.debitAccount[debitAccountN] = {
-      debitCurrency : debitCurrency || "UAH", //тип валюты UAH, RUB, USD, GBP, EUR и другие
-      debitBalance  : debitBalance || 0, //текущий баланс, положителен либо нулевой
-      dateEndActive : dateEndActive, //дата активности карты
-    };
+
+  setDebitAccount (date, balance, currency) {
+    this.debitAccount.push({
+      currency : currency || "UAH",
+      balance  : balance || 0,
+      dateEndActive : date,
+    });
   }
-  setCreditAccount (dateEndActive, creditFunds, creditLimit, creditCurrency) {
-    this.creditAccount = {
-      creditCurrency : creditCurrency || "UAH", //тип валюты UAH, RUB, USD, GBP, EUR и другие
-      creditBalance : creditFunds || 0, //кредитные средства
-      creditLimit : creditLimit || 0, //кредитный лимит
-      dateEndActive : dateEndActive, //дата активности карты
-    };
+  setCreditAccount (date, balance, limit, currency) {
+    this.creditAccount.push({
+      currency : currency || "UAH",
+      balance : balance || 0,
+      limit : limit || 0,
+      dateEndActive : date,
+    });
   }
 }
-let customerOne  = new Customer('vasya', true, '12.09.2021')
-let customerTwo  = new Customer('petya', true, '12.09.2021')
-let customerThree  = new Customer('valya', false, '12.09.2021')
-let customerFour  = new Customer('masha', true, '12.09.2021')
-let customerFive  = new Customer('olya', true, '12.09.2021')
-customerOne.setDebitAccount('debit1','25.12.2023', 500)
-customerOne.setDebitAccount('debit2','25.12.2023', 500)
-customerOne.setDebitAccount('debit3','25.12.2023', 500)
-customerOne.setCreditAccount('25.12.2023', 250, 200)
+
+let customerOne  = new Customer('vasya', true, '12.09.2021');
+let customerTwo  = new Customer('petya', true, '12.09.2021');
+let customerThree  = new Customer('valya', false, '12.09.2021');
+let customerFour  = new Customer('masha', true, '12.09.2021');
+let customerFive  = new Customer('olya', true, '12.09.2021');
+customerOne.setDebitAccount('25.12.2023', 500);
+customerOne.setDebitAccount('25.12.2023', 500);
+customerOne.setDebitAccount('25.12.2023', 500)
+customerOne.setCreditAccount('25.12.2023', 150, 200);
+customerOne.setCreditAccount('25.12.2023', 250, 500)
+customerThree.setCreditAccount('25.12.2023', 150, 200);
+customerThree.setCreditAccount('25.12.2023', 250, 500);
+customerTwo.setCreditAccount('25.12.2023', 400, 500, "USA");
+customerTwo.setCreditAccount('25.12.2023', 300, 500, "RUR");
+customerTwo.setCreditAccount('25.12.2023', 200, 500, "EUR");
+customerTwo.setCreditAccount('25.12.2023', 100, 500, 'USA');
 
 bank.push(customerOne,customerTwo,customerThree,customerFour,customerFive);
 
-let debtBank = function() {
+let creditDutyAllCustomers = async function() {
   let duty = 0;
-  bank.forEach((item => {
-    if (item.creditAccount && (item.creditAccount.creditLimit > item.creditAccount.creditBalance)) {
-      duty += (item.creditAccount.creditLimit - item.creditAccount.creditBalance);
-    }
-  }))
+  await getExchangeRates()
+    .then(res => {
+      bank.forEach(((item)=> {
+        if (item.creditAccount.length) {
+          item.creditAccount.forEach(item => {
+            if (item.limit > item.balance){
+              if(item.currency === "UAH"){
+                duty += (item.limit - item.balance) / res[0].sale;
+              } else if(item.currency === "RUR"){
+                duty += (item.limit - item.balance) * res[2].sale / res[0].sale;
+              } else if(item.currency === "EUR"){
+                duty += (item.limit - item.balance) * res[1].sale / res[0].sale;
+              } else {
+                duty += (item.limit - item.balance);
+              }
+            }
+          })
+        }
+      }));
+    })
+
   return duty;
 };
 
 console.log(bank)
-console.log(debtBank())
+console.log(creditDutyAllCustomers())
 
 
