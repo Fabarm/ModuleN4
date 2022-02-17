14ac29121589243d183c0d78d3242e0979540428
fabarm <leonovxai@gmail.com>
2022-02-17 10:24:15 +0200

    added class for customers

--- bank.js
+++ bank.js
@@ -1,119 +1,62 @@
-let bank = [
-  {
-    fullName : 'Sidorov S.S.',
-    isActive : true,
-    dateReg : '13.09.2013',
-    bankAccount : 'debit',
-    currencyType : 'UAH',
-    balance : 1500,
-    endActiveCard : '13.09.2023',
-  },
-  {
-    fullName : 'Petrov P.P.',
-    isActive : true,
-    dateReg : '23.01.2015',
-    bankAccount : 'credit',
-    currencyType : 'UAH',
-    balance : 2500,
-    creditLimit : 1000,
-    endActiveCard : '03.05.2025',
-  },
-  {
-    fullName : 'Ivanov I.I.',
-    isActive : true,
-    dateReg : '10.10.2010',
-    bankAccount : 'debit',
-    currencyType : 'USA',
-    balance : 3500,
-    endActiveCard : '30.09.2024',
-  },
-  {
-    fullName : 'Ivanenko I.I.',
-    isActive : true,
-    dateReg : '15.01.2018',
-    bankAccount : 'debit',
-    currencyType : 'USA',
-    balance : 1500,
-    endActiveCard : '03.03.2023',
-  },
-  {
-    fullName : 'Petrichenko P.P.',
-    isActive : true,
-    dateReg : '25.11.2020',
-    bankAccount : 'credit',
-    currencyType : 'UAH',
-    balance : 3500,
-    creditLimit : 1500,
-    endActiveCard : '13.10.2025',
-  },
-  {
-    fullName : 'Sidorenko I.I.',
-    isActive : true,
-    dateReg : '05.05.2019',
-    bankAccount : 'debit',
-    currencyType : 'USA',
-    balance : 800,
-    endActiveCard : '07.06.2028',
-  },
-  {
-    fullName : 'Stepanenko P.P.',
-    isActive : false,
-    dateReg : '25.11.2020',
-    bankAccount : 'credit',
-    currencyType : 'UAH',
-    balance : 1000,
-    creditLimit : 1500,
-    endActiveCard : '13.10.2025',
-  },
-  {
-    fullName : 'Stepanenko P.P.',
-    isActive : true,
-    dateReg : '05.11.2019',
-    bankAccount : 'credit',
-    currencyType : 'USA',
-    balance : 700,
-    creditLimit : 1500,
-    endActiveCard : '28.01.2023',
-  },
-];
+let bank = [];
+
+async function getExchangeRates (url) {
+  let res = await fetch(url);
+
+  if (!res.ok) {
+    throw new Error(`Could not fetch ${url}, status: ${res.status}`);
+  }
+
+  return await res.json();
+}
+// let a1 = getExchangeRates("https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=5")
+// console.log(a1)
+
+class Customer {
+  fullName;
+  isActive;
+  dateReg;
+  debitAccount;
+  creditAccount;
+  constructor(fullName, isActive, dateReg) {
+    this.fullName = fullName;
+    this.isActive = isActive;
+    this.dateReg = dateReg;
+  }
+  setDebitAccount (dateEndActive, debitBalance, debitCurrency) {
+    this.debitAccount = {
+      debitCurrency : debitCurrency || "UAH", //тип валюты UAH, RUB, USD, GBP, EUR и другие
+      debitBalance  : debitBalance || 0, //текущий баланс, положителен либо нулевой
+      dateEndActive : dateEndActive, //дата активности карты
+    };
+  }
+  setCreditAccount (dateEndActive, creditFunds, creditLimit, creditCurrency) {
+    this.creditAccount = {
+      creditCurrency : creditCurrency || "UAH", //тип валюты UAH, RUB, USD, GBP, EUR и другие
+      creditBalance : creditFunds || 0, //кредитные средства
+      creditLimit : creditLimit || 0, //кредитный лимит
+      dateEndActive : dateEndActive, //дата активности карты
+    };
+  }
+}
+let customerOne  = new Customer('vasya', true, '12.09.2021')
+let customerTwo  = new Customer('petya', true, '12.09.2021')
+let customerThree  = new Customer('valya', false, '12.09.2021')
+let customerFour  = new Customer('masha', true, '12.09.2021')
+let customerFive  = new Customer('olya', true, '12.09.2021')
+customerOne.setDebitAccount('25.12.2023', 250)
+customerOne.setCreditAccount('25.12.2023', 300, 500, "USA")
+
+bank.push(customerOne,customerTwo,customerThree,customerFour,customerFive);
 
 let debtBank = function() {
-  let duty = {};
+  let duty = 0;
   bank.forEach((item => {
-    if (item.bankAccount === 'credit') {
-      if((item.creditLimit - item.balance) > 0){
-        if (duty[item.currencyType] === undefined) {
-          duty[item.currencyType] = (item.creditLimit - item.balance);
-        } else {
-          duty[item.currencyType] += (item.creditLimit - item.balance);
-        }
-      }
+    if (item.creditAccount && (item.creditAccount.creditLimit > item.creditAccount.creditBalance)) {
+      duty += (item.creditAccount.creditLimit - item.creditAccount.creditBalance);
     }
   }))
  return duty;
 };
 
-function assets()  {
-  fetch("https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=5")
-    .then(data => data.json())
-    .then(res => {
-      let sum = 0;
-      bank.forEach(item => {
-        if (item.currencyType === "UAH") {
-          item.balance /= res[0].buy;
-        }
-        sum += item.balance;
-      });
-      let obj = debtBank();
-      let totalSum = obj.USA + obj.UAH / res[0].buy;
-      document.querySelector(".total").textContent = `Общее количество денег внутри банка - ${sum.toFixed(2)} $`;
-      document.querySelector(".duty").textContent = `Сумма долга клиентов - ${totalSum.toFixed(2)} $`;
-    });
-}
-assets();
-
-// fetch("https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=5")
-//   .then(data => data.json())
-//   .then(res => console.log(res))
-
-
+console.log(debtBank())
