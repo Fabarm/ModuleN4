98ec8071f039987f27d23e31c497d2673326fa77
fabarm <leonovxai@gmail.com>
2022-02-17 10:45:53 +0200

    test

--- bank.js
+++ bank.js
@@ -18,13 +18,15 @@ class Customer {
   dateReg;
   debitAccount;
   creditAccount;
+
   constructor(fullName, isActive, dateReg) {
     this.fullName = fullName;
     this.isActive = isActive;
     this.dateReg = dateReg;
+    this.debitAccount = {}
   }
-  setDebitAccount (dateEndActive, debitBalance, debitCurrency) {
-    this.debitAccount = {
+  setDebitAccount (debitAccountN, dateEndActive, debitBalance, debitCurrency) {
+    this.debitAccount[debitAccountN] = {
       debitCurrency : debitCurrency || "UAH", //тип валюты UAH, RUB, USD, GBP, EUR и другие
       debitBalance  : debitBalance || 0, //текущий баланс, положителен либо нулевой
       dateEndActive : dateEndActive, //дата активности карты
@@ -44,8 +46,10 @@ let customerTwo  = new Customer('petya', true, '12.09.2021')
 let customerThree  = new Customer('valya', false, '12.09.2021')
 let customerFour  = new Customer('masha', true, '12.09.2021')
 let customerFive  = new Customer('olya', true, '12.09.2021')
-customerOne.setDebitAccount('25.12.2023', 250)
-customerOne.setCreditAccount('25.12.2023', 300, 500, "USA")
+customerOne.setDebitAccount('debit1','25.12.2023', 500)
+customerOne.setDebitAccount('debit2','25.12.2023', 500)
+customerOne.setDebitAccount('debit3','25.12.2023', 500)
+customerOne.setCreditAccount('25.12.2023', 250, 200)
 
 bank.push(customerOne,customerTwo,customerThree,customerFour,customerFive);
 
@@ -56,7 +60,10 @@ let debtBank = function() {
       duty += (item.creditAccount.creditLimit - item.creditAccount.creditBalance);
     }
   }))
- return duty;
+  return duty;
 };
 
+console.log(bank)
 console.log(debtBank())
+
+
